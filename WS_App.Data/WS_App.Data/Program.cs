74712d0f30fe9bd7419c91ce6f539a11fbdab39a
Saket Tripathi <sak.tri123@gmail.com>
2015-08-07 02:37:04 +0530

    Begin Logs n Service Layer

---
+++ WS_App.Data/WS_App.Data/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WS_App.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
